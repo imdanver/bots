3af30ddea3b2b29225e37ee4703d1883d0ee274c
imdanver <danver.box@gmail.com>
2019-12-20 12:27:45 +0200

    1 commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
 
-let srv = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type" : "text/plain"})
-    res.write("Hello, world!")
-    res.end()
-})
+function load(){
+    let srv = http.createServer(function(req, res){
+        if(req.url === "/"){
+            res.writeHead(200, {"Content-Type" : "text/html;charset=UTF-8"})
+            res.write(fs.readFileSync(path.join(__dirname, 'index.html')))
+            res.end()
+        }
+        if(req.url === "/data"){
+            let fullDate = new Date()
+            let dataBefore = fullDate.getUTCHours() + ':' + fullDate.getUTCMinutes() + ':' + fullDate.getUTCSeconds()
+            fs.appendFileSync(path.join(__dirname, 'trading_log.txt'), dataBefore + os.EOL, 'utf8')
+            let dataAfter = fs.readFileSync(path.join(__dirname, 'trading_log.txt'))
+            res.write(dataAfter + os.EOL)
+            res.end()
+        }
+    })
 
-srv.listen(8080)
+    const PORT = process.env.PORT || 8080
+    srv.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
 
-console.log('Listening on port 8080')
+}
+
+load()
+
